a51867e4d1c49b41fe81e03cc5a1787daceb7c14
atrox3d <atrox3d@gmail.com>
2021-02-12 18:43:15 +0100

    detect collision with wall, my solution

--- ball.py
+++ ball.py
@@ -7,8 +7,15 @@ class Ball(Turtle):
         super().__init__("square")
         self.color("white")
         self.penup()
+        self.ystep = -10
 
     def move(self):
+        if self.ycor() >= 300 - 10:
+            self.ystep = -10
+        elif self.ycor() <= -300 + 10:
+            self.ystep = 10
+
         x = self.xcor() + 10
-        y = self.ycor() + 10
+        y = self.ycor() + self.ystep
+
         self.goto(x, y)
