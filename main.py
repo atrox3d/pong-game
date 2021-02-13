95d8fe8a28d67b29088cc578893c99a3d4de8e50
atrox3d <atrox3d@gmail.com>
2021-02-13 05:40:14 +0100

    add constants, tune bounce

--- main.py
+++ main.py
@@ -3,21 +3,68 @@ from turtle import Screen
 
 from ball import Ball
 from paddle import Paddle
-
+#
+#   screen
+#
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+#
+#   paddles
+#
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 100
+RPADDLE_X = 350
+LPADDLE_X = -350
+RPADDLE_POSITION = RPADDLE_X, 0
+LPADDLE_POSITION = LPADDLE_X, 0
+#
+#   ball
+#
+BALL_SIZE = 20
+HALF_BALL = BALL_SIZE / 2
+#
+#   upper and lower walls
+#
+Y_WALL_CENTER_DISTANCE = (SCREEN_HEIGHT / 2)
+Y_WALL_BALL_LIMIT = Y_WALL_CENTER_DISTANCE - HALF_BALL
+UPPER_WALL = Y_WALL_BALL_LIMIT
+LOWER_WALL = Y_WALL_BALL_LIMIT * -1
+#
+#   left and right wall
+#
+X_WALL_CENTER_DISTANCE = (SCREEN_WIDTH / 2)
+X_WALL_BALL_LIMIT = X_WALL_CENTER_DISTANCE - HALF_BALL
+RIGHT_WALL = X_WALL_BALL_LIMIT
+LEFT_WALL = X_WALL_BALL_LIMIT * -1
+#
+#   collision
+#
+BALL_PADDLE_COLLISION = 50
+RPADDLE_WALL = RPADDLE_X - (PADDLE_WIDTH / 2 )
+LPADDLE_WALL = LPADDLE_X - -(PADDLE_WIDTH / 2 )
+#
+#   misc
+#
+SLEEP_TIME = 0.08
 
-RPADDLE_POSITION = 350, 0
-LPADDLE_POSITION = -350, 0
-
-BALL_WIDTH = 20
-BALL_HEIGHT = 20
-
-WALL_CENTER_DISTANCE = (SCREEN_HEIGHT / 2)
-WALL_BALL_LIMIT = WALL_CENTER_DISTANCE - (BALL_HEIGHT / 2)
-NORTH_WALL = WALL_BALL_LIMIT
-SOUTH_WALL = WALL_BALL_LIMIT * -1
-
+print(f"SCREEN_WIDTH           : {SCREEN_WIDTH            }")
+print(f"SCREEN_HEIGHT          : {SCREEN_HEIGHT           }")
+print(f"LPADDLE_POSITION       : {LPADDLE_POSITION        }")
+print(f"RPADDLE_POSITION       : {RPADDLE_POSITION        }")
+print(f"BALL_SIZE              : {BALL_SIZE               }")
+print(f"HALF_BALL              : {HALF_BALL               }")
+print(f"Y_WALL_CENTER_DISTANCE : {Y_WALL_CENTER_DISTANCE  }")
+print(f"Y_WALL_BALL_LIMIT      : {Y_WALL_BALL_LIMIT       }")
+print(f"UPPER_WALL             : {UPPER_WALL              }")
+print(f"LOWER_WALL             : {LOWER_WALL              }")
+print(f"X_WALL_CENTER_DISTANCE : {X_WALL_CENTER_DISTANCE  }")
+print(f"X_WALL_BALL_LIMIT      : {X_WALL_BALL_LIMIT       }")
+print(f"RIGHT_WALL             : {RIGHT_WALL              }")
+print(f"LEFT_WALL              : {LEFT_WALL               }")
+print(f"BALL_PADDLE_COLLISION  : {BALL_PADDLE_COLLISION   }")
+print(f"RPADDLE_WALL           : {RPADDLE_WALL            }")
+print(f"LPADDLE_WALL           : {LPADDLE_WALL            }")
+print(f"SLEEP_TIME             : {SLEEP_TIME              }")
 
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
@@ -40,17 +87,24 @@ screen.onkey(lpaddle.down, "s")
 play = True
 while play:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(SLEEP_TIME)
     ball.move()
     #
     #   wall collision
     #
-    if ball.ycor() >= NORTH_WALL:
+    if ball.ycor() >= UPPER_WALL:
         ball.ybounce()
-    elif ball.ycor() <= SOUTH_WALL:
+    elif ball.ycor() <= LOWER_WALL:
         ball.ybounce()
     #
     #   rpaddle collision
     #
+    if ball.distance(rpaddle) < BALL_PADDLE_COLLISION and ball.xcor() >= RPADDLE_WALL:
+        ball.xbounce()
+    #
+    #   rpaddle collision
+    #
+    if ball.distance(lpaddle) < BALL_PADDLE_COLLISION and ball.xcor() <= LPADDLE_WALL:
+        ball.xbounce()
 
 screen.exitonclick()
